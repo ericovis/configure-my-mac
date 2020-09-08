ca782b86fb52d95ec58aef7ee3b794157960c0de
Eric Magalhães <eric@emagalha.es>
2020-09-08 17:43:59 -0300

    updated playbook and bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,10 @@
 
 set -e 
 
-
-pip3 install ansible --user
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install python@3.8
+ln -s /usr/local/bin/python3 /usr/local/bin/python
+ln -s /usr/local/bin/pip3 /usr/local/bin/pip
+ln -s /usr/local/bin/python3-config /usr/local/bin/python-config
+pip3 install ansible
